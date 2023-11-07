b6dcb6c8dce7d5eff6a5d7db9e4670a620155177
abfotev20 <abfotev20@codingburgas.bg>
2023-11-07 10:39:08 +0200

    7.11.23

---
+++ src/Java.java
@@ -0,0 +1,16 @@
+import _7_11_23.Child;
+
+import java.util.Scanner;
+public class Java {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        int age = Integer.parseInt(scanner.nextLine());
+        Child child = new Child(name, age);
+        System.out.println(child.getName());
+        System.out.println(child.getAge());
+
+    }
+}
